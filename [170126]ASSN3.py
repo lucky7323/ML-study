f6e252a0b6d7180167c82fb15147e0ddcc0e30a1
eunho <lucky7323@postech.ac.kr>
2017-01-27 00:35:25 +0900

    Fix initialize, 75%

--- [170126]ASSN3.py
+++ [170126]ASSN3.py
@@ -39,10 +39,10 @@ test_images  = parse_images('data/t10k-images-idx3-ubyte.gz')
 test_labels  = parse_labels('data/t10k-labels-idx1-ubyte.gz')
 
 # parameters
-batch_size = 500
-epochs = 15
+batch_size = 200
+epochs = 100
 learning_rate = 0.001
-drop_rate = 0.3
+drop_rate = 0.5
 hidden_size = 256
 
 # falttening x & convert one-hot vector y
@@ -51,16 +51,19 @@ y_ = np.zeros((60000,10))
 y_[np.arange(60000), train_labels] = 1
 
 # weights and bias
-W1 = np.random.rand(784,hidden_size)
-W2 = np.random.rand(hidden_size,hidden_size)
-W3 = np.random.rand(hidden_size,10)
+W1 = 2 * np.random.rand(784,hidden_size) - 1
+W2 = 2 * np.random.rand(hidden_size,hidden_size) - 1
+W3 = 2 * np.random.rand(hidden_size,10) - 1
 
-B1 = np.random.rand(hidden_size)
-B2 = np.random.rand(hidden_size)
-B3 = np.random.rand(10)
+B1 = 2 * np.random.rand(hidden_size) - 1
+B2 = 2 * np.random.rand(hidden_size) - 1
+B3 = 2 * np.random.rand(10) - 1
 
 # plotting variable
 arr = np.zeros(epochs)
+
+def ReLU(x):
+   return np.maximum(0, x)
 
 # batch function
 def batch(x, y, n):
@@ -74,10 +77,10 @@ def lsoftmax(x):
 
 # cross-entropy function
 def cross_entropy(batch_xs, batch_ys, W1, W2, W3, B1, B2, B3):
-    H1 = np.maximum(0, np.matmul(batch_xs, W1) + B1)
+    H1 = ReLU(np.matmul(batch_xs, W1)+B1)
     D1 = np.random.rand(*H1.shape) > drop_rate
     H1 *= D1
-    H2 = np.maximum(0, np.matmul(H1, W2) + B2)
+    H2 = ReLU(np.matmul(H1, W2) + B2)
     D2 = np.random.rand(*H2.shape) > drop_rate
     H2 *= D2
     y = np.matmul(H2, W3) + B3
@@ -101,6 +104,7 @@ for i in range(epochs):
     B2 -= grad_fun_B2(batch_xs, batch_ys, W1, W2, W3, B1, B2, B3) * learning_rate
     B3 -= grad_fun_B3(batch_xs, batch_ys, W1, W2, W3, B1, B2, B3) * learning_rate
     arr[i] = cross_entropy(batch_xs, batch_ys, W1,W2,W3,B1,B2,B3)
+    print(i+1, ": %0.2f" % arr[i])
 
 # plotting loss function graph
 plt.plot(arr, 'r--')
@@ -111,8 +115,8 @@ x_test = test_images.reshape([10000, 28*28])
 y_answer = np.zeros((10000,10))
 y_answer[np.arange(10000), test_labels] = 1
 
-H1_ = np.maximum(0, np.matmul(x_test, W1) + B1)
-H2_ = np.maximum(0, np.matmul(H1_, W2) + B2)
+H1_ = ReLU(np.matmul(x_test, W1) + B1)
+H2_ = ReLU(np.matmul(H1_, W2) + B2)
 y_predict = np.matmul(H2_, W3) + B3
 
 accuracy = np.mean(np.equal(np.argmax(y_predict,1), np.argmax(y_answer,1))) * 100
