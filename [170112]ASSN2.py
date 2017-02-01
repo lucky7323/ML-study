97567fd9c31369f633c359bdaa9e748ab249596a
eunho <lucky7323@postech.ac.kr>
2017-02-01 16:54:41 +0900

    data preprocessing, 91.5%

--- [170112]ASSN2.py
+++ [170112]ASSN2.py
@@ -38,7 +38,13 @@ train_labels = parse_labels('data/train-labels-idx1-ubyte.gz')
 test_images  = parse_images('data/t10k-images-idx3-ubyte.gz')
 test_labels  = parse_labels('data/t10k-labels-idx1-ubyte.gz')
 
-# convert to one-hot vector
+# parameters
+learning_rate = 0.5
+batch_size = 100
+epoch = 1000
+
+# data flattening & convert to one-hot vector
+x = train_images.reshape([60000, 28*28])
 y_ = np.zeros((60000,10))
 y_[np.arange(60000), train_labels] = 1
 
@@ -47,66 +53,44 @@ def lsoftmax(x):
     z = x-x.max(1,keepdims=True)
     return (z - np.log(np.sum(np.exp(z),axis=1,keepdims=True)))
 
-# falttening
-x = train_images.reshape([60000, 28*28])
-
-# cross-entropy (random)
+# cross-entropy
 def cross_entropy(W, b):            
-    ran = 100 * (np.random.random_integers(1,600) - 1)
-    x_sample = x[ran + np.arange(100)]
-    y_sample = y_[ran + np.arange(100)]
+    ran = batch_size * (np.random.random_integers(1,60000/batch_size) - 1)
+    x_sample = x[ran + np.arange(batch_size)]
+    y_sample = y_[ran + np.arange(batch_size)]
+    x_sample = x_sample / 255.0
     y = np.matmul(x_sample,W) + b
     return np.mean(-np.sum(y_sample * lsoftmax(y), axis=1))
 
-# cross-entropy (sequence)
-def cross_entropy_(W, b, n):
-    x_sample = x[n*100 + np.arange(200)]
-    y_sample = y_[n*100 + np.arange(200)]
-    y = np.matmul(x_sample,W) + b
-    return np.mean(-np.sum(y_sample * lsoftmax(y), axis=1))
-
+# gradient function
 grad_fun_W = grad(cross_entropy, argnum=0)
 grad_fun_b = grad(cross_entropy, argnum=1)
-grad_fun_W1 = grad(cross_entropy_, argnum=0)
-grad_fun_b1 = grad(cross_entropy_, argnum=1)
 
-W0 = np.zeros((784,10))
-b0 = np.zeros(10)
-W1 = np.zeros((784,10))
-b1 = np.zeros(10)
-W2 = np.zeros((784,10))
-b2 = np.zeros(10)
+# weight and bias initialization
+W = np.zeros((784,10))
+b = np.zeros(10)
 
-arr1 = np.zeros(11) 
-arr2 = np.zeros(11)
+# plotting variable
+arr = np.zeros(epoch) 
 
-#training (random)
-for i in range(1000):
-    W1 -= grad_fun_W(W1,b0) * 0.5
-    b1 -= grad_fun_b(W0,b1) * 0.5
-    if i%100==0 or i==1000:
-        arr1[int(i/100)] = cross_entropy(W1,b1)
-plt.plot(arr1, 'r--')
+# training
+for i in range(epoch):
+    W -= grad_fun_W(W,b) * learning_rate
+    b -= grad_fun_b(W,b) * learning_rate
+    arr[i] = cross_entropy(W,b)
+    if (i+1)%100 == 0:
+        # evaluating
+        x_test = test_images.reshape([10000, 28*28])
+        x_test = x_test / 255.0
+        y_answer = np.zeros((10000,10))
+        y_answer[np.arange(10000), test_labels] = 1
+        y_predict = np.matmul(x_test,W) + b
+        accuracy = np.mean(np.equal(np.argmax(y_predict,1), np.argmax(y_answer,1))) * 100
+        print('{} epoch: {:.2f}, cost: {:.2f}'.format(i+1, accuracy, arr[i]))
+        if (i+1)==epoch:
+            print('Accuracy : {:.2f}'.format(accuracy))
+
+# plotting cost function value
+plt.plot(arr, 'r--')
 plt.show()
- 
-#training (sequence)
-for i in range(1000):
-    W2 -= grad_fun_W1(W2,b0,i%300) * 0.5
-    b2 -= grad_fun_b1(W0,b2,i%300) * 0.5
-    if i%100==0 or i==1000:
-        arr2[int(i/100)] = cross_entropy(W2,b2)
-plt.plot(arr2, 'r--')
-plt.show()
-
-#evaluating
-x_test = test_images.reshape([10000, 28*28])
-y_answer = np.zeros((10000,10))
-y_answer[np.arange(10000), test_labels] = 1
-y_predict1 = np.matmul(x_test,W1) + b1
-y_predict2 = np.matmul(x_test,W2) + b2
-accuracy1 = np.mean(np.equal(np.argmax(y_predict1,1), np.argmax(y_answer,1))) * 100
-accuracy2 = np.mean(np.equal(np.argmax(y_predict2,1), np.argmax(y_answer,1))) * 100
-
-print("Accuracy (random) : %0.2f%%" % accuracy1)
-print("Accuracy (order) : %0.2f%%" % accuracy2)
 
